084c0f1e97c32f3d5c0579a7117ed536f35b8bc9
Poteau Florian <florianpoteau59@gmail.com>
2023-09-12 14:44:28 +0200

    feat(token)

--- lib/services/headers/header.dart
+++ lib/services/headers/header.dart
@@ -4,6 +4,6 @@ class Header {
   final Dio _dio;
 
   Header(Object authToken) : _dio = Dio() {
-    final hi = _dio.options.headers['Authorization'] = 'Bearer $authToken';
+    _dio.options.headers['Authorization'] = 'Bearer $authToken';
   }
 }
