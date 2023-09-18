9bd1d4298fc9f08fc234a70ecc03f40c1751a8aa
Poteau Florian <florianpoteau59@gmail.com>
2023-09-18 13:00:30 +0200

    feat(filter dropdown page SeancePublic)

--- lib/screen/widget/containerCardSport.dart
+++ lib/screen/widget/containerCardSport.dart
@@ -1,16 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:renconsport/screen/widget/customCardSport.dart';
+import 'package:renconsport/screen/widget/Card/cardSeanceFiltre.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ContainerCardSport extends StatelessWidget {
-  const ContainerCardSport(
-      {super.key, required selectedSport, required Color cardColor});
+  final Color cardColor;
+  final String? selectedSport;
+
+  ContainerCardSport(
+      {this.cardColor = const Color(0xFF0091AD), this.selectedSport});
+
+  List<Widget> _buildCards() {
+    List<Widget> cards = [
+      CardSeanceFiltre(
+          content: 'Card 1',
+          iconData: FontAwesomeIcons.running,
+          cardColor: cardColor),
+      CardSeanceFiltre(
+          content: 'Card 2',
+          iconData: FontAwesomeIcons.dumbbell,
+          cardColor: cardColor),
+    ];
+
+    if (selectedSport == 'Tout') {
+      print("Returning all cards");
+      return cards;
+    } else if (selectedSport == 'Musculation') {
+      print("Filtering for Musculation");
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.dumbbell)
+          .toList();
+    } else if (selectedSport == 'Athlétisme') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.running)
+          .toList();
+    } else if (selectedSport == 'Natation') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.swimmer)
+          .toList();
+    } else if (selectedSport == 'Football') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.futbol)
+          .toList();
+    } else if (selectedSport == 'Basket-ball') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData ==
+              FontAwesomeIcons.baseballBall)
+          .toList();
+    } else if (selectedSport == 'Tennis') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData ==
+              FontAwesomeIcons.tableTennis)
+          .toList();
+    } else if (selectedSport == 'Cyclisme') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.biking)
+          .toList();
+    } else if (selectedSport == 'Arts martiaux') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.userNinja)
+          .toList();
+    } else if (selectedSport == 'Yoga') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.peace)
+          .toList();
+    } else if (selectedSport == 'Danse') {
+      return cards
+          .where((card) =>
+              (card as CardSeanceFiltre).iconData == FontAwesomeIcons.music)
+          .toList();
+    }
+    return cards;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.center,
       child: Container(
-        width: MediaQuery.of(context).size.width,
+        width: 380,
         height: 630,
         margin: EdgeInsets.only(top: 20),
         decoration: BoxDecoration(
@@ -23,13 +99,7 @@ class ContainerCardSport extends StatelessWidget {
             padding: EdgeInsets.only(top: 20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                CustomCardSport(
-                  content: 'Card 1',
-                ),
-                SizedBox(height: 20),
-                CustomCardSport(content: 'Card 2')
-              ],
+              children: _buildCards(),
             ),
           ),
         ),
