dbf1bed3d47d83bc9d68bf825c373b6c1fa42f8e
Poteau Florian <florianpoteau59@gmail.com>
2023-10-29 21:24:03 +0100

    fix(lien d'api qui changer car l'api est héberger de sorte à éviter d'utiliser le réseau)

--- lib/services/ApiConfig/apiConfig.dart
+++ lib/services/ApiConfig/apiConfig.dart
@@ -1,3 +1,4 @@
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.0.34:8000/api';
+  static const String baseUrl =
+      'https://574f-2a01-e0a-2c2-ded0-f87e-f9ca-71e8-9786.ngrok-free.app/api';
 }
